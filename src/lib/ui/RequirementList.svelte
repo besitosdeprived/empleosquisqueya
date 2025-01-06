d8f4d443c9e119d1891cb02cf7d59a609f19ff77
Crstphr <184663573+besitosdeprived@users.noreply.github.com>
2025-01-06 10:17:32 -0400

    add: RequirementList component

---
+++ src/lib/ui/RequirementList.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  // biome-ignore lint/style/useConst: <explanation>
+  let requirements = $state<string[]>([]);
+  let input: HTMLInputElement;
+  function addItem() {
+    if (input.value === "") {
+      return;
+    }
+    requirements.push(input.value);
+    input.value = "";
+  }
+</script>
+
+<div>
+  <div>
+    <input type="text" bind:this={input} />
+    <button type="button" onclick={addItem}>Add</button>
+  </div>
+  <ul>
+    {#each requirements as requirement}
+      <li>{requirement}</li>
+      <input
+        type="checkbox"
+        value={requirement}
+        name="requirements"
+        checked
+        hidden
+      />
+    {/each}
+  </ul>
+</div>
